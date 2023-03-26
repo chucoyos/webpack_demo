ae8818c311431e2d937f10459a486ea4f569efe0
unodev <unodev@outlook.com>
2023-03-26 03:21:03 -0600

    Add style and css loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import './style.css';
 
 // function comp() {
 //   const ele = document.createElement('div')
@@ -9,4 +10,5 @@ import _ from 'lodash'
 // document.body.appendChild(comp())
 const element = document.createElement('div')
 element.innerHTML = _.join(['Hello', 'webpack', 'without function'], ' ')
+element.classList.add('hello');
 document.body.appendChild(element)
