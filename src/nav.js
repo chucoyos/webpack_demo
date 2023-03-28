44b6a431683b9ebb9679e0e8814b641cfceb9eff
unodev <unodev@outlook.com>
2023-03-27 23:45:30 -0600

    Add about page

--- src/nav.js
+++ src/nav.js
@@ -2,13 +2,12 @@ import right from "./right";
 
 const nav = () => {
   const nav = document.createElement('nav');
-  // right().appendChild(nav);
   nav.classList.add('nav');
   
   const navList = document.createElement('ul');
   navList.classList.add('nav-list');
   nav.appendChild(navList);
-  const navItems = ['Home', 'Menu', 'Contact'];
+  const navItems = ['Home', 'Menu', 'About'];
   navItems.forEach((item) => {
     const navItem = document.createElement('li');
     navItem.classList.add('nav-item');
@@ -20,16 +19,20 @@ const nav = () => {
       switch (item.target.id) {
         case 'Home':
           mainTitle.textContent = '1751';
+          mainSubtitle.classList.remove('main-subtitle-about');
+          mainSubtitle.classList.add('main-subtitle');
           mainSubtitle.textContent = 'SEA and BAR';
         break;
         case 'Menu':
           mainTitle.textContent = 'menu';
+          mainSubtitle.classList.remove('main-subtitle-about');
+          mainSubtitle.classList.add('main-subtitle');
           mainSubtitle.textContent = 'menu';
-          break;
-        case 'Contact':
-          mainTitle.textContent = 'contact';
-          mainSubtitle.textContent = 'contact';
-          break;
+        break;
+        case 'About':
+          mainTitle.textContent = 'about';
+          mainSubtitle.classList.add('main-subtitle-about');
+          mainSubtitle.textContent = '1751 offers seasonally inspired menus featuring the highest quality seafood and meats available from the local Gulf coast and beyond. Our full raw bar features several different varieties of oysters and rare delicacies such as and O-toro fatty tuna, Uni, Imperial Golden Ossetra caviar, soft shell crab, and local stone crab claws when available.';
         break;
       }
     })
